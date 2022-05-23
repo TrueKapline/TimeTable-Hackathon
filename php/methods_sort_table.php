3faab39f124e8d9d235614431a0a756d10e3796c
Ilya Novoselov <my.mypew@mail.ru>
2022-05-23 14:59:55 +0300

    Добавил сортировку таблицы по дате

--- php/methods_sort_table.php
+++ php/methods_sort_table.php
@@ -29,7 +29,7 @@ require "alteration_table.php";
   * * "год-месяц-день" - формат входящего значения
   * {количество_дней}:
   - Количество дней, которое мы хотим вернуть, начиная с указанной даты
-  * * количество - целочисленное число от 1 до 90
+  * * количество - целочисленное число от 1
   * {номер_пары}:
   - Номер пары, которую мы хотим получить
   * * номер - целочисленное число от 0 до 8; 0 - если хотим вернуть все пары в дне
@@ -72,23 +72,26 @@ require "alteration_table.php";
   как сортировать расписание по входным данным запроса */
 function sort_schedule($data) {
   $sort_schedule = output_table($data->table);
-
-  table_correct_date($sort_schedule, $data->date, $data->date_count);
+  
+  table_correct_date($sort_schedule, $data->date, $data->day_count);
   table_correct_pair($sort_schedule, $data->pair_number);
-  table_sort($sort_schedule, $data->type_sort);
   table_correct_keys($sort_schedule, $data->output, $data->output_key);
   table_correct_type($sort_schedule, $data->type);
   table_correct_type_lessons($sort_schedule, $data->type_lessons);
   table_correct_subgroup_number($sort_schedule, $data->subgroup_number);
+  table_sort($sort_schedule, $data->type_sort);
 
   return $sort_schedule;
 }
 
 /* Метод, который оставляет в таблице только даты начиная с заданной даты с заданным количеством дней */
-function table_correct_date(&$table, $date, $date_count) {
-  $date_end = fulldate_date_p_day($date, $date_count);
+function table_correct_date(&$table, $date, $day_count) {
+  if ($date == null || $day_count == null) return $table;
 
-  for($i = 0; $i < count($table); $i++) {
+  $date_end = fulldate_date_p_day($date, $day_count);
+  $size_table = count($table);
+
+  for($i = 0; $i < $size_table; $i++) {
     if(strcmp($table[$i]->date, $date) < 0 || strcmp($table[$i]->date, $date_end) > 0) {
       unset($table[$i]);
     }
@@ -99,32 +102,82 @@ function table_correct_date(&$table, $date, $date_count) {
 
 /* Метод, который оставляет в таблице дни только с заданным номером пар  */
 function table_correct_pair(&$table, $pair_number) {
+  if ($pair_number == null || $pair_number === 0) return $table;
 
+  $size_table = count($table);
+
+  for($i = 0; $i < $size_table; $i++) {
+    if($table[$i]->pair != $pair_number) {
+      unset($table[$i]);
+    }
+  }
+
+  $table = array_values($table);
 }
 
 /* Метод, который сортирует таблицу с заданным типом сортировки */
 function table_sort(&$table, $type_sort) {
+  if ($type_sort == null) return $table;
 
+  switch ($type_sort) {
+    case "date":
+      date_sort($table);
+      break;
+    case "groups":
+      break;
+    case "teachers":
+      break;
+    case "auditories":
+      break;
+  }
 }
 
 /* Метод, который оставляет в таблице пары только с заданными ключами */
 function table_correct_keys(&$table, $output, $output_key) {
+  if ($output == null || $output_key == null) return $table;
 
 }
 
 /* Метод, который оставляет в таблице пары только с заданным типом */
 function table_correct_type(&$table, $type) {
+  if ($type == null) return $table;
 
 }
 
 /* Метод, который оставляет в таблице пары только с заданным типом занятия */
 function table_correct_type_lessons(&$table, $type_lessons) {
+  if ($type_lessons == null) return $table;
 
 }
 
 /* Метод, который оставляет в таблице занятия лабораторные только с заданным номером группы*/
 function table_correct_subgroup_number(&$table, $subgroup_number) {
+  if ($subgroup_number == null) return $table;
 
+}
+
+/* Метод, который сортирует таблицу по дате */
+function date_sort(&$table) {
+  $count = 0;
+  
+  while($count < count($table)-1) {
+    if( strcmp($table[$count]->date, $table[$count+1]->date) > 0 ||
+        strcmp($table[$count]->date, $table[$count+1]->date) === 0 && $table[$count]->pair > $table[$count+1]->pair) {
+      $swap = $table[$count];
+      $table[$count] = $table[$count+1];
+      $table[$count+1] = $swap;
+
+      $count = -1;
+    }
+
+    $count++;
+  }
+}
+
+/* Метод, который сортирует таблицу по группам */
+function groups_sort(&$table) {
+  $count = 0;
+  
 }
 
 /* Метод, который ищет пару по заданному id и возвращает её индекс, если не находят, возвращает -1 */
