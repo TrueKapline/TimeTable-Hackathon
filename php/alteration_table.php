523bd3cf642d859c0617f6ad58069cbdd9a8b0fe
Ilya Novoselov <my.mypew@mail.ru>
2022-05-24 16:09:29 +0300

    Добавил атрибуты описания, если пара была перенесена и исправил ошибки

--- php/alteration_table.php
+++ php/alteration_table.php
@@ -1,9 +1,5 @@
 <?php
 
-//require "time.php"; // ВРЕМЕННО. УДАЛИТЬ
-//require "real_time_schedule.php"; // ВРЕМЕННО. УДАЛИТЬ
-//require "methods_sort_table.php";// ВРЕМЕННО. УДАЛИТЬ
-
 /* Метод, который принимает на вход номер таблицы и возвращает эту таблицу в виде объекта */
 function output_table($table, $date, $day_count) {
   switch ($table) {
@@ -14,83 +10,9 @@ function output_table($table, $date, $day_count) {
       return json_decode(file_get_contents("../queries.json"));             // Расписание мероприятий, проектов, переносов
       break;
     case 3:
-      return create_real_time_table($date, $day_count);         // Раписания реального времени
+      return create_real_time_table($date, $day_count);                     // Раписания реального времени
       break;
   }
-}
-
-function test($date, $day_count) {
-  $table_1 = output_table(1, null, null);
-  $table_2 = output_table(2, null, null);
-  $table_3 = [];
-  
-  while($day_count > 0) {
-    $day = day($date);
-    $week = week($date)%2 + 1;
-
-    /* Создаём расписание начиная с указаной даты и с указанным количеством дней, согласно 1 таблице */
-    for ($i = 0; $i < count($table_1); $i++) {
-      if($table_1[$i]->day === $day && $table_1[$i]->week === $week) {
-        $real_time_schedule = new real_time_schedule($table_1[$i]->type,
-                                          $table_1[$i]->type_lessons,
-                                          generateID($date, $table_1[$i]->id),
-                                          $table_1[$i]->pair,
-                                          $day,
-                                          $week,
-                                          $date,
-                                          $table_1[$i]->groups,
-                                          $table_1[$i]->subgroup_number,
-                                          $table_1[$i]->title,
-                                          $table_1[$i]->teachers,
-                                          $table_1[$i]->auditories,
-                                          $table_1[$i]->transfer,
-                                          $table_1[$i]->related_queries,
-                                          $table_1[$i]->comment);
-        adder($table_3, $real_time_schedule);   
-      }
-    }
-
-    /* Добавляем данные из 2 таблицы в 3, если соответсвует указанной дате и количеству дней*/
-    for ($i = 0; $i < count($table_2); $i++) {
-      /* Для мероприятий и проектов */
-      if ($table_2[$i]->type < 3 && $table_2[$i]->dt === $date) {
-        $real_time_schedule = new real_time_schedule($table_2[$i]->type+1,
-                                            null,
-                                            $table_2[$i]->id,
-                                            $table_2[$i]->pair,
-                                            day($table_2[$i]->dt),
-                                            week($table_2[$i]->dt),
-                                            $table_2[$i]->dt,
-                                            $table_2[$i]->groups,
-                                            null,
-                                            $table_2[$i]->description,
-                                            $table_2[$i]->teachers,
-                                            $table_2[$i]->auditories,
-                                            false,
-                                            null,
-                                            null);
-        adder($table_3, $real_time_schedule);
-      }
-      /* Для удаления пары */
-      elseif($table_2[$i]->type === 3) {
-        $s_id = search_id($table_3, $table_2[$i]->affected_schedule_id);
-
-        if($s_id != -1) {
-          unset($table_3[$s_id]);
-          $table_3 = array_values($table_3);
-        }
-      } 
-      /* Для добавления пары */
-      elseif($table_2[$i]->type === 4) {
-        
-      }
-    }
-
-    $date = fulldate_date_p_day($date, 2);
-    $day_count--;
-  }
-
-  file_put_contents("../test.json", json_encode($table_3, JSON_UNESCAPED_UNICODE));
 }
 
 /* Метод, который создаёт таблицу, исходя из данных 1 и 2 таблиц и возвращает её  */
@@ -118,8 +40,8 @@ function create_real_time_table($date, $day_count) {
                                           $table_1[$i]->title,
                                           $table_1[$i]->teachers,
                                           $table_1[$i]->auditories,
-                                          $table_1[$i]->transfer,
-                                          $table_1[$i]->related_queries,
+                                          $table_1[$i]->transfer_type,
+                                          $table_1[$i]->id_family,
                                           $table_1[$i]->comment);
         adder($table_3, $real_time_schedule);   
       }
@@ -134,30 +56,52 @@ function create_real_time_table($date, $day_count) {
                                             $table_2[$i]->id,
                                             $table_2[$i]->pair,
                                             day($table_2[$i]->dt),
-                                            week($table_2[$i]->dt),
+                                            week($table_2[$i]->dt)%2 + 1,
                                             $table_2[$i]->dt,
                                             $table_2[$i]->groups,
                                             null,
                                             $table_2[$i]->description,
                                             $table_2[$i]->teachers,
                                             $table_2[$i]->auditories,
-                                            false,
+                                            0,
                                             null,
                                             null);
         adder($table_3, $real_time_schedule);
       }
-      /* Для удаления пары */
-      elseif($table_2[$i]->type === 3) {
+      /* Для пары, которая переносится с данной даты */
+      elseif($table_2[$i]->type === 3 && $table_2[$i]->dt === $date) {
         $s_id = search_id($table_3, $table_2[$i]->affected_schedule_id);
 
         if($s_id != -1) {
-          unset($table_3[$s_id]);
-          $table_3 = array_values($table_3);
+          $table_3[$s_id]->transfer_type = 1;
+          $table_3[$s_id]->id_family[0] = $table_3[$s_id]->id;
+          $table_3[$s_id]->id_family[1] = $table_2[search_id($table_2, $table_2[$i]->related_queries[1])]->affected_schedule_id;
+          $table_3[$s_id]->comment = $table_2[$i]->description;
         }
       } 
-      /* Для добавления пары */
-      elseif($table_2[$i]->type === 4) {
-        
+      /* Для пары, которая переносится на данную дату */
+      elseif($table_2[$i]->type === 4 && $table_2[$i]->dt === $date) {
+        $s_id = search_id($table_3, $table_2[search_id($table_2, $table_2[$i]->related_queries[1])]->affected_schedule_id);
+        $array = [];
+        $array[0] = $table_2[$i]->affected_schedule_id;
+        $array[1] = $table_2[search_id($table_2, $table_2[$i]->related_queries[1])]->affected_schedule_id;
+
+        $real_time_schedule = new real_time_schedule($table_3[$s_id]->type,
+                                          $table_3[$s_id]->type_lessons,
+                                          $table_2[$i]->affected_schedule_id,
+                                          $table_2[$i]->pair,
+                                          day($table_2[$i]->dt),
+                                          week($table_2[$i]->dt)%2 + 1,
+                                          $table_2[$i]->dt,
+                                          $table_2[$i]->groups,
+                                          $table_3[$s_id]->subgroup_number,
+                                          $table_3[$s_id]->title,
+                                          $table_2[$i]->teachers,
+                                          $table_2[$i]->auditories,
+                                          2,
+                                          $array,
+                                          $table_2[$i]->description);
+        adder($table_3, $real_time_schedule); 
       }
     }
 
