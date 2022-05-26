a8aefc57ecfdaea6a89260ecc6091f91f480ae48
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2022-05-26 22:52:05 +0800

    Перенесен календарь из реакта

--- 123.js
+++
@@ -1,6 +0,0 @@
-function daySelected(msg) {
-    var logElem = document.querySelector(".date__day");
-    
-    let dayStr = this.date.getDate();
-    document.getElementsByClassName("date__day").value = dayStr;
-}
